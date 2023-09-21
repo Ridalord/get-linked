6cef29ae2077d54779b01a6d474aeabd2d378dc9
Ridalord <ridhwan.alabi@gmail.com>
2023-09-21 14:06:56 +0100

    Intro Section Complete

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -6,7 +6,7 @@ import IntroContent from "./IntroContent";
 export default function IntroSection() {
   return (
     <>
-      <SectionTemplate topHeader="Introduction to getlinked" buttomHeader="tech Hackathon 1.0" sectionContent={<IntroContent />} sectionImage="./bigIdea.png" />
+      <SectionTemplate topHeader="Introduction to getlinked" buttomHeader="tech Hackathon 1.0" sectionContent={<IntroContent />} sectionImage="./bigIdea.png" reversed={false} />
     </> 
   )
 }
