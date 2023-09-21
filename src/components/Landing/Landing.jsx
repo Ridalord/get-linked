1c3c2fb9b87fa1b9c92cf7d001db425b06e803a5
Ridalord <ridhwan.alabi@gmail.com>
2023-09-21 10:24:37 +0100

    Landing pagr desktop complete

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -36,7 +36,7 @@ export default function Landing() {
             </div>
             <div>
               <svg xmlns="http://www.w3.org/2000/svg" width="117" height="11" viewBox="0 0 117 11" fill="none">
-                <path d="M1 9C20.2424 3.71764 70.7273 -3.67768 116 9" stroke="#FF26B9" stroke-width="3" />
+                <path d="M1 9C20.2424 3.71764 70.7273 -3.67768 116 9" stroke="#FF26B9" strokeWidth="3" />
               </svg>
             </div>
           </div>
