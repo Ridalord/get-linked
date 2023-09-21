36cc32cf6da4baa38bc36d3ae6b7562e2b23a0ae
Ridalord <ridhwan.alabi@gmail.com>
2023-09-21 11:32:52 +0100

    Section template created

---
+++ src/components/SectionTemplate/SectionTemplate.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import classes from "./SectionTemplate.module.css"
+
+export default function SectionTemplate(props) {
+  return (
+    <section>
+      <div className={classes.sectionImage} style={{ backgroundImage: `url(${props.sectionImage})` }}></div>
+      <div className={classes.sectionDetails}>
+        <div className={classes.sectionHeading}>
+          <span>{props.topHeader}</span>
+          <span className={classes.coloredHeader}>{props.buttomHeader}</span>
+        </div>
+        <div className={classes.sectionContent}>
+          {props.sectionContent}
+        </div>
+      </div>
+    </section>
+  )
+}
