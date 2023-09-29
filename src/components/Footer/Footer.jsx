3c0bf148e1994620d1ffb9d417724e5be3155b00
Ridalord <ridhwan.alabi@gmail.com>
2023-09-29 15:21:13 +0100

    Footer complete desktop

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,23 +4,24 @@ import Instagram from "./insta.svg"
 import X from "./x.svg"
 import Linkedin from "./linkedIn.svg"
 import Facebook from "./facebook.svg"
+import classes from "./Footer.module.css"
 
 
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
+    <footer className={`${classes.footer} d-flex justify-content-between`}>
+      <div className="col-sm-12 col-lg-5 d-flex justify-content-between">
         <div>
           <Logo />
-          <p>Getlinked Tech Hackathon is a technology innovation program established by a group of organizations with the aim of showcasing young and talented individuals in the field of technology.</p>
+          <p className={classes.logoSub}>Getlinked Tech Hackathon is a technology innovation program established by a group of organizations with the aim of showcasing young and talented individuals in the field of technology.</p>
         </div>
-        <div>
+        <div className={classes.termsPolicy}>
           <a href="#tou">Terms of Use</a>
           <a href="#prvcyplcy">Privacy Policy</a>
         </div>
       </div>
-      <ul>
+      <ul className={`${classes.footerList} col-lg-3 col-sm-12 d-flex`}>
         <li>
           Useful Links
         </li>
@@ -36,7 +37,7 @@ export default function Footer() {
         <li>
           <a href="#register">Register</a>
         </li>
-        <li>
+        <li className={`${classes.followUs} d-flex`}>
           <span>Follow us</span>
           <span>
             <a href="#insta">
@@ -60,6 +61,16 @@ export default function Footer() {
           </span>
         </li>
       </ul>
+      <ul className={`${classes.footerList} col-lg-2 col-sm-12 d-flex ${classes.contact}`}>
+        <li>Contact Us</li>
+        <li>
+          <a href="#tel">+234 6707653444</a>
+        </li>
+        <li>
+          <a href="#address">27,Alara Street Yaba 100012 Lagos State</a>
+        </li>
+      </ul>
+      <p className={`${classes.copyright} col-12 text-center`}>All rights reserved. &copy; getlinked Ltd.</p>
     </footer>
   )
 }
